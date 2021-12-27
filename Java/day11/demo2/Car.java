e52b557fa79c15cd59f05e29e8e00ee736d7ac44
张少为 <2729273955@qq.com>
2021-12-27 13:02:08 +0000

    day11

--- Java/day11/demo2/Car.java
+++ Java/day11/demo2/Car.java
@@ -2,18 +2,21 @@ package Java.day11.demo2;
 
 public class Car {
     boolean status;
-    
-    class Engine {
-        
-    }
 
-    public void work() {
-        
+    class Engine {
+        public void work() {
+            if (getStatus() == true) {
+                System.out.println("发动机飞速旋转中");
+            } else {
+                System.out.println("发动机停止工作");
+            }
+        }
     }
 
     public boolean getStatus() {
         return status;
     }
+
     public void setStatus(boolean status) {
         this.status = status;
     }
---
+++ Java/day11/demo2/Test.java
@@ -0,0 +1,8 @@
+package Java.day11.demo2;
+
+public class Test {
+    public static void main(String[] args) {
+        Car c = new Car();
+        
+    }
+}
