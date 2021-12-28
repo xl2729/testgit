97366d5bb4d37b3289f1cdc734eccc4c375b6d11
张少为 <2729273955@qq.com>
2021-12-28 08:14:19 +0000

    内置内置函数函数接口接口

--- Java/day12/demo1/Demo1.java
+++ Java/day12/demo1/Demo1.java
@@ -1,5 +1,18 @@
 package Java.day12.demo1;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
+/**
+ * Supplier接口　供给　必须有一个返回值
+ */
 public class Demo1 {
-    
+    public static void main(String[] args) {
+        Supplier<Integer> supplier = ()->{
+            return new Random().nextInt(101)+100;
+        };
+
+        Integer result = supplier.get();
+        System.out.println("随机产生一个100-200的整数："+result);
+    }
 }
